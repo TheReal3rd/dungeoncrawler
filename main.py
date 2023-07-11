91f90c952da51fdac60f5934e3f155fe51726619
³ʳᵈ <60966545+TheReal3rd@users.noreply.github.com>
2023-07-11 13:58:11 +0100

    Progress

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+#TODO list
+# 1. Create Inventory system.
+# 2. Create enemies. (Multiple types with different behaviour)
+# 3. Complete the game world.
+# 4. Add player animations.
+#Low Prio list
+# 1. 
+
+
 # START Items
 # Item list:
-# Explination: okay Classes don't work properly so i can't create OOP like item definitions so im going to use ids.
+# Explination: okay Classes don't work properly so i can't create OOP like item definitions so im going to use ids. (Its gonna be kinda slow)
 # 0 = None
 # 1 = HealthPotion
 # 2 = Burger
+# 3 = Iron_Sword (TODO)
+# 4 = Wooden_Shield (TODO)
+# 5 = Iron_Armour_Set (TODO)
 def getImage(itemID):
     if itemID == 1:# Health pot
         return assets.image("""HealthItem""")
@@ -14,7 +26,7 @@ def getImage(itemID):
 
 
 def useItem(itemID):
-    if itemID in (1, 2):# Health pot
+    if itemID in (1, 2):# Health pot and burger
         if(useCondition(itemID)):
             info.change_life_by(2)
             if(info.life() >= 6):
@@ -30,8 +42,35 @@ def useCondition(itemID):
     else:# None
         return True
 
-
 # END Items
+#START Inventory
+
+def offScreen():
+    global inventorySprite, buttons
+    inventorySprite.set_position(-1000, -1000)
+    for x in range(0, 4):
+        buttons[x].set_position(-1000, -1000)
+            
+
+def onScreen():
+    global inventorySprite, buttons
+    pos = spriteToScreen(inventorySprite)
+    inventorySprite.x = pos[0]
+    inventorySprite.y = pos[1]
+
+    yOffset = 23
+    for x in range(0, 4):
+        pos = spriteToScreen(buttons[x])
+        buttons[x].x = pos[0] + (scene.screen_width() - buttons[x].width)
+        buttons[x].y = pos[1] + yOffset
+        yOffset += 18
+
+        if inventorySlot == x:
+            buttons[x].set_image(assets.image("""inventoryButtonLit"""))
+        else:
+            buttons[x].set_image(assets.image("""inventoryButton0""")) 
+
+#END Inventory
 # START Utils
 def clamp(minNum: number, maxNum: number, value: number):
     if value < minNum:
@@ -44,11 +83,16 @@ def calcDistance(posX1: number, posY1: number, posX2: number, posY2: number):
     yDiff = posY1 - posY2
     return Math.sqrt(xDiff * xDiff + yDiff * yDiff)
 def spriteToScreen(textSprite: Sprite):
-    # #TODO remove the hardcoded values.
-    return [clamp(80, 3120, playerOne.x) - (scene.screen_width() - textSprite.width) / 2,
-        clamp(116,
-            3196,
-            playerOne.y + (scene.screen_height() - textSprite.height) / 2)]
+    # X_range = 80
+    # Y_range = 60
+    # tile_size = 16
+    # map_size = ? (200)
+    # tile_size * map_size - (X_range or Y_range)
+    #So no real point making it dynamic as you can't.
+    return [
+        clamp(80, 3120, playerOne.x) - (scene.screen_width() - textSprite.width) / 2,
+        clamp(60, 3140, playerOne.y) - (scene.screen_height() - textSprite.height) / 2
+        ]
 class msDelay():#This breaks blocks. and its annoying.
     time = 0
 
@@ -66,87 +110,140 @@ class msDelay():#This breaks blocks. and its annoying.
             return False
 
 # END Utils
+#Kind defs
 @namespace
 class SpriteKind:
     Item = SpriteKind.create()
+@namespace
+class SpriteKind:
+    Inventory = SpriteKind.create()
+
+#Main instructions
 def executeAction(actionID: number):
+    global inventoryOpen
     # Health Potion TODO add a notify system that pops up to inform the user why they can't drink or use an item.
     if actionID == 0:# Inventory
-        print("Inventory Not implmented")
+        if inventoryOpenDelay.passedMS(200):
+            inventoryOpen = not inventoryOpen
+            print("Open")
+
     elif actionID == 1:# Attack
         print("Attack Not implemented")
     elif actionID == 2:
         useItem(1)
-# for item in playerInventory:
-# if item.getItemName() == "HealthPotion":
-# if item.canUse():
-# item.useItem()
-# print("Used item")
+
 def updatePlayer():
-    global actionSelectIndex, playerAction
-    # START Movement
-    if controller.up.is_pressed():
-        playerOne.y += playerSpeed * -1
-    if controller.down.is_pressed():
-        playerOne.y += playerSpeed
-    if controller.right.is_pressed():
-        playerOne.x += playerSpeed
-    if controller.left.is_pressed():
-        playerOne.x += playerSpeed * -1
-    # END Movement
-    if controller.A.is_pressed():
-        # Use Actions
-        executeAction(actionSelectIndex)
-    if controller.B.is_pressed():
-        # Actions
-        if actionSwapDelay.passedMS(500):
-            actionSelectIndex += 1
-            if actionSelectIndex >= 3:
-                actionSelectIndex = 0
-            playerAction.destroy()
-            playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
-            playerAction.set_flag(SpriteFlag.AUTO_DESTROY, True)
-    textPos = spriteToScreen(playerAction)
-    playerAction.x = textPos[0]
-    playerAction.y = textPos[1]
+    global actionSelectIndex, playerAction, inventoryOpen, inventorySprite, actionSwapDelay, buttons, inventorySlot, inventoryInputDelay
+    #Inventory
+    if inventoryOpen:
+        onScreen()
+
+        if inventoryInputDelay.passedMS(250):
+            if controller.up.is_pressed():
+                inventorySlot = inventorySlot - 1
+            elif controller.down.is_pressed():
+                inventorySlot = inventorySlot + 1
+
+        inventorySlot = clamp(0, 3, inventorySlot)
+
+        if controller.B.is_pressed(): 
+            if inventoryOpenDelay.passedMS(200):
+                inventoryOpen = False
+                actionSwapDelay.reset()
+                return
+        elif controller.A.is_pressed():
+            pass
+            #TODO add use item functionlity
+
+    else:
+        offScreen();
+
+        # START Movement
+        if controller.up.is_pressed():
+            playerOne.y += playerSpeed * -1
+        elif controller.down.is_pressed():
+            playerOne.y += playerSpeed
+        if controller.right.is_pressed():
+            playerOne.x += playerSpeed
+        elif controller.left.is_pressed():
+            playerOne.x += playerSpeed * -1
+        # END Movement
+        if controller.A.is_pressed():
+            # Use Actions
+            executeAction(actionSelectIndex)
+        if controller.B.is_pressed():
+            # Actions
+            if actionSwapDelay.passedMS(500):
+                actionSelectIndex += 1
+                if actionSelectIndex >= 3:
+                    actionSelectIndex = 0
+                playerAction.destroy()
+                playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
+                playerAction.set_flag(SpriteFlag.AUTO_DESTROY, True)
+
+        #Action Text
+        textPos = spriteToScreen(playerAction)
+        playerAction.x = textPos[0]
+        playerAction.y = textPos[1] + (scene.screen_height() - playerAction.height)
+
+
+
 # This will update all nearby enemies alongside load them in and out.
 # So we check where the player is and if an enemy should be their spawn it in if it's not done already. 
-# The range idk why as its best to be hard coded in instead.
 def updateEntities():
     pass
 def collisionCheck():
     pass
+
+#START of on start
 # Consts
 maxNumItems = 4
 actionsStrings = ["Inventory", "Attack", "Health Potion"]
 # Their is a minimap extension that i could use? maybe get the core game in then start adding features. This is to test the consoles limits.
 scene.set_background_color(2)
 actionSelectIndex = 0
-playerLevel = 1
-playerSpeed = 0
-playerOne: Sprite = None
-playerAction: TextSprite = None
-playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
 # This not working ill use ids instead. Inheritance might be broken or something.
 playerInventory = [0, 0, 0, 0]
 levelID = 0
 tiles.set_current_tilemap(tilemap("""
     level1
 """))
+
+info.set_life(3)
+game.stats = True
+actionSwapDelay = msDelay()
+#Player
+playerSpeed = 1
+playerLevel = 1
+playerSpeed = 0
 playerOne = sprites.create(assets.image("""
     PlayerIdle
 """), SpriteKind.player)
-playerSpeed = 1
 scene.camera_follow_sprite(playerOne)
-info.set_life(1)
-game.stats = True
-actionSwapDelay = msDelay()
+playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
+#Inventory Vars
+inventorySprite = sprites.create(assets.image("""inventoryBG"""), SpriteKind.Inventory)
+inventoryOpenDelay = msDelay()
+inventoryInputDelay = msDelay()
 inventoryOpen = False
-# playerOne.x = 3000
-# playerOne.y = 3000
+inventorySlot = 0
+buttons = [
+    sprites.create(assets.image("""inventoryButton0"""), SpriteKind.Inventory),
+    sprites.create(assets.image("""inventoryButton0"""), SpriteKind.Inventory),
+    sprites.create(assets.image("""inventoryButton0"""), SpriteKind.Inventory),
+    sprites.create(assets.image("""inventoryButton0"""), SpriteKind.Inventory)
+]
+
+
+#Buttons END
+#playerOne.x = 3000
+#playerOne.y = 3000
+#END of on start
 
 def on_forever():
     updatePlayer()
     updateEntities()
     collisionCheck()
+
+    #print(scene.screen_width() +" " + scene.screen_height())
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+// TODO list
+//  1. Create Inventory system.
+//  2. Create enemies. (Multiple types with different behaviour)
+//  3. Complete the game world.
+//  4. Add player animations.
+// Low Prio list
+//  1. 
 //  START Items
 //  Item list:
-//  Explination: okay Classes don't work properly so i can't create OOP like item definitions so im going to use ids.
+//  Explination: okay Classes don't work properly so i can't create OOP like item definitions so im going to use ids. (Its gonna be kinda slow)
 //  0 = None
 //  1 = HealthPotion
 //  2 = Burger
+//  3 = Iron_Sword (TODO)
+//  4 = Wooden_Shield (TODO)
+//  5 = Iron_Armour_Set (TODO)
 function getImage(itemID: any): Image {
     if (itemID == 1) {
         //  Health pot
@@ -20,7 +30,7 @@ function getImage(itemID: any): Image {
 
 function useItem(itemID: number) {
     if ([1, 2].indexOf(itemID) >= 0) {
-        //  Health pot
+        //  Health pot and burger
         if (useCondition(itemID)) {
             info.changeLifeBy(2)
             if (info.life() >= 6) {
@@ -48,6 +58,36 @@ function useCondition(itemID: number): boolean {
 }
 
 //  END Items
+// START Inventory
+function offScreen() {
+    
+    inventorySprite.setPosition(-1000, -1000)
+    for (let x = 0; x < 4; x++) {
+        buttons[x].setPosition(-1000, -1000)
+    }
+}
+
+function onScreen() {
+    
+    let pos = spriteToScreen(inventorySprite)
+    inventorySprite.x = pos[0]
+    inventorySprite.y = pos[1]
+    let yOffset = 23
+    for (let x = 0; x < 4; x++) {
+        pos = spriteToScreen(buttons[x])
+        buttons[x].x = pos[0] + (scene.screenWidth() - buttons[x].width)
+        buttons[x].y = pos[1] + yOffset
+        yOffset += 18
+        if (inventorySlot == x) {
+            buttons[x].setImage(assets.image`inventoryButtonLit`)
+        } else {
+            buttons[x].setImage(assets.image`inventoryButton0`)
+        }
+        
+    }
+}
+
+// END Inventory
 //  START Utils
 function clamp(minNum: number, maxNum: number, value: number): number {
     if (value < minNum) {
@@ -68,8 +108,13 @@ function calcDistance(posX1: number, posY1: number, posX2: number, posY2: number
 }
 
 function spriteToScreen(textSprite: Sprite): number[] {
-    //  #TODO remove the hardcoded values.
-    return [clamp(80, 3120, playerOne.x) - (scene.screenWidth() - textSprite.width) / 2, clamp(116, 3196, playerOne.y + (scene.screenHeight() - textSprite.height) / 2)]
+    //  X_range = 80
+    //  Y_range = 60
+    //  tile_size = 16
+    //  map_size = ? (200)
+    //  tile_size * map_size - (X_range or Y_range)
+    // So no real point making it dynamic as you can't.
+    return [clamp(80, 3120, playerOne.x) - (scene.screenWidth() - textSprite.width) / 2, clamp(60, 3140, playerOne.y) - (scene.screenHeight() - textSprite.height) / 2]
 }
 
 class msDelay {
@@ -112,15 +157,26 @@ class msDelay {
 msDelay.__initmsDelay()
 
 //  END Utils
+// Kind defs
 namespace SpriteKind {
     export const Item = SpriteKind.create()
 }
 
+namespace SpriteKind {
+    export const Inventory = SpriteKind.create()
+}
+
+// Main instructions
 function executeAction(actionID: number) {
+    
     //  Health Potion TODO add a notify system that pops up to inform the user why they can't drink or use an item.
     if (actionID == 0) {
         //  Inventory
-        console.log("Inventory Not implmented")
+        if (inventoryOpenDelay.passedMS(200)) {
+            inventoryOpen = !inventoryOpen
+            console.log("Open")
+        }
+        
     } else if (actionID == 1) {
         //  Attack
         console.log("Attack Not implemented")
@@ -130,59 +186,80 @@ function executeAction(actionID: number) {
     
 }
 
-//  for item in playerInventory:
-//  if item.getItemName() == "HealthPotion":
-//  if item.canUse():
-//  item.useItem()
-//  print("Used item")
 function updatePlayer() {
+    let textPos: number[];
     
-    //  START Movement
-    if (controller.up.isPressed()) {
-        playerOne.y += playerSpeed * -1
-    }
-    
-    if (controller.down.isPressed()) {
-        playerOne.y += playerSpeed
-    }
-    
-    if (controller.right.isPressed()) {
-        playerOne.x += playerSpeed
-    }
-    
-    if (controller.left.isPressed()) {
-        playerOne.x += playerSpeed * -1
-    }
-    
-    //  END Movement
-    if (controller.A.isPressed()) {
-        //  Use Actions
-        executeAction(actionSelectIndex)
-    }
-    
-    if (controller.B.isPressed()) {
-        //  Actions
-        if (actionSwapDelay.passedMS(500)) {
-            actionSelectIndex += 1
-            if (actionSelectIndex >= 3) {
-                actionSelectIndex = 0
+    // Inventory
+    if (inventoryOpen) {
+        onScreen()
+        if (inventoryInputDelay.passedMS(250)) {
+            if (controller.up.isPressed()) {
+                inventorySlot = inventorySlot - 1
+            } else if (controller.down.isPressed()) {
+                inventorySlot = inventorySlot + 1
             }
             
-            playerAction.destroy()
-            playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
-            playerAction.setFlag(SpriteFlag.AutoDestroy, true)
         }
         
+        inventorySlot = clamp(0, 3, inventorySlot)
+        if (controller.B.isPressed()) {
+            if (inventoryOpenDelay.passedMS(200)) {
+                inventoryOpen = false
+                actionSwapDelay.reset()
+                return
+            }
+            
+        } else if (controller.A.isPressed()) {
+            
+        }
+        
+    } else {
+        // TODO add use item functionlity
+        offScreen()
+        //  START Movement
+        if (controller.up.isPressed()) {
+            playerOne.y += playerSpeed * -1
+        } else if (controller.down.isPressed()) {
+            playerOne.y += playerSpeed
+        }
+        
+        if (controller.right.isPressed()) {
+            playerOne.x += playerSpeed
+        } else if (controller.left.isPressed()) {
+            playerOne.x += playerSpeed * -1
+        }
+        
+        //  END Movement
+        if (controller.A.isPressed()) {
+            //  Use Actions
+            executeAction(actionSelectIndex)
+        }
+        
+        if (controller.B.isPressed()) {
+            //  Actions
+            if (actionSwapDelay.passedMS(500)) {
+                actionSelectIndex += 1
+                if (actionSelectIndex >= 3) {
+                    actionSelectIndex = 0
+                }
+                
+                playerAction.destroy()
+                playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
+                playerAction.setFlag(SpriteFlag.AutoDestroy, true)
+            }
+            
+        }
+        
+        // Action Text
+        textPos = spriteToScreen(playerAction)
+        playerAction.x = textPos[0]
+        playerAction.y = textPos[1] + (scene.screenHeight() - playerAction.height)
     }
     
-    let textPos = spriteToScreen(playerAction)
-    playerAction.x = textPos[0]
-    playerAction.y = textPos[1]
 }
 
 //  This will update all nearby enemies alongside load them in and out.
 //  So we check where the player is and if an enemy should be their spawn it in if it's not done already. 
-//  The range idk why as its best to be hard coded in instead.
 function updateEntities() {
     
 }
@@ -191,34 +268,43 @@ function collisionCheck() {
     
 }
 
+// START of on start
 //  Consts
 let maxNumItems = 4
 let actionsStrings = ["Inventory", "Attack", "Health Potion"]
 //  Their is a minimap extension that i could use? maybe get the core game in then start adding features. This is to test the consoles limits.
 scene.setBackgroundColor(2)
 let actionSelectIndex = 0
-let playerLevel = 1
-let playerSpeed = 0
-let playerOne : Sprite = null
-let playerAction : TextSprite = null
-playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
 //  This not working ill use ids instead. Inheritance might be broken or something.
 let playerInventory = [0, 0, 0, 0]
 let levelID = 0
 tiles.setCurrentTilemap(tilemap`
     level1
 `)
-playerOne = sprites.create(assets.image`
-    PlayerIdle
-`, SpriteKind.Player)
-playerSpeed = 1
-scene.cameraFollowSprite(playerOne)
-info.setLife(1)
+info.setLife(3)
 game.stats = true
 let actionSwapDelay = new msDelay()
+// Player
+let playerSpeed = 1
+let playerLevel = 1
+playerSpeed = 0
+let playerOne = sprites.create(assets.image`
+    PlayerIdle
+`, SpriteKind.Player)
+scene.cameraFollowSprite(playerOne)
+let playerAction = textsprite.create(actionsStrings[actionSelectIndex], 10, 15)
+// Inventory Vars
+let inventorySprite = sprites.create(assets.image`inventoryBG`, SpriteKind.Inventory)
+let inventoryOpenDelay = new msDelay()
+let inventoryInputDelay = new msDelay()
 let inventoryOpen = false
-//  playerOne.x = 3000
-//  playerOne.y = 3000
+let inventorySlot = 0
+let buttons = [sprites.create(assets.image`inventoryButton0`, SpriteKind.Inventory), sprites.create(assets.image`inventoryButton0`, SpriteKind.Inventory), sprites.create(assets.image`inventoryButton0`, SpriteKind.Inventory), sprites.create(assets.image`inventoryButton0`, SpriteKind.Inventory)]
+// Buttons END
+// playerOne.x = 3000
+// playerOne.y = 3000
+// END of on start
+// print(scene.screen_width() +" " + scene.screen_height())
 forever(function on_forever() {
     updatePlayer()
     updateEntities()
